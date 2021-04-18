f9c42c907270e8771bee75a576b1c4a374421d1d
Petar Milovanovic <petar97m@gmail.com>
2021-04-18 13:11:13 +0200

    Implement the use of JWT

---
+++ config/jwt.secret.ts
@@ -0,0 +1,5 @@
+export const jwtSecret: string = '93401ghn84931hf183jf1fg=1gj3190gh' +
+                                 'g8342hg89432hf9-804hf981hf91h94hf' +
+                                 'gh892435hgbn8942-5hbg89542h9=f103' +
+                                 'gj9542jg92hg90=3jgkbtmjj2=4gb38=5' +
+                                 'vb013465h01dxn81f5g0918fc9ccfnhgb';
--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -19,6 +19,18 @@ export class AdministratorService {
         return this.administrator.find();
     }
 
+    async getByUsername(usernameString: string): Promise<Administrator | null> {
+        const admin = await this.administrator.findOne({
+            username: usernameString
+        });
+
+        if (admin) {
+            return admin;
+        }
+
+        return null;
+    }
+
     getById(id: number): Promise<Administrator> {
         return this.administrator.findOne(id);
     }
