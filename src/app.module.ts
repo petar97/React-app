6071ea0a4ce92987e551a46f46e69c371ef16a0a
Petar Milovanovic <petar97m@gmail.com>
2021-04-28 11:47:43 +0200

    Unify user and administrator tokens and implement user login

--- src/app.module.ts
+++ src/app.module.ts
@@ -81,6 +81,7 @@ import { UserService } from './services/user/user.service';
   ],
   exports: [
     AdministratorService,
+    UserService,
   ],
 })
 export class AppModule implements NestModule {
--- src/controllers/api/auth.controller.ts
+++ src/controllers/api/auth.controller.ts
@@ -3,13 +3,14 @@ import { LoginAdministratorDto } from "src/dtos/administrator/login.administrato
 import { ApiResponse } from "src/misc/api.response.class";
 import { AdministratorService } from "src/services/administrator/administrator.service";
 import * as crypto from 'crypto';
-import { LoginInfoAdministratorDto } from "src/dtos/administrator/login.info.administrator.dto";
+import { LoginInfoDto } from "src/dtos/auth/login.info.dto";
 import * as jwt from 'jsonwebtoken';
-import { JwtDataAdministratorDto } from "src/dtos/administrator/jwt.data.administrator.dto";
+import { JwtDataDto } from "src/dtos/auth/jwt.data.dto";
 import { Request } from "express";
 import { jwtSecret } from "config/jwt.secret";
 import { UserRegistrationDto } from "src/dtos/user/user.registration.dto";
 import { UserService } from "src/services/user/user.service";
+import { LoginUserDto } from "src/dtos/user/login.user.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -18,11 +19,11 @@ export class AuthController {
         public userService: UserService
     ) {}
 
-    @Post('login')
-    async doLogin(
+    @Post('administrator/login')
+    async doAdministratorLogin(
         @Body() data: LoginAdministratorDto, 
         @Req() req: Request
-    ): Promise<LoginInfoAdministratorDto | ApiResponse> {
+    ): Promise<LoginInfoDto | ApiResponse> {
         const administrator = await this.administratorService.getByUsername(data.username);
 
         if (!administrator) {
@@ -37,9 +38,10 @@ export class AuthController {
             return new Promise(resolve => resolve(new ApiResponse('error', -3002)));
         }
 
-        const jwtData = new JwtDataAdministratorDto();
-        jwtData.administratorId = administrator.administratorId;
-        jwtData.username = administrator.username;
+        const jwtData = new JwtDataDto();
+        jwtData.role = "administrator";
+        jwtData.id = administrator.administratorId;
+        jwtData.identity = administrator.username;
 
         let sada = new Date();
         sada.setDate(sada.getDate() + 14);
@@ -51,7 +53,7 @@ export class AuthController {
 
         let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret);
 
-        const responseObject = new LoginInfoAdministratorDto(
+        const responseObject = new LoginInfoDto(
             administrator.administratorId,
             administrator.username,
             token
@@ -64,4 +66,48 @@ export class AuthController {
     async userRegister(@Body() data: UserRegistrationDto) {
         return await this.userService.register(data);
     }
+
+    @Post('user/login')
+    async doUserLogin(
+        @Body() data: LoginUserDto, 
+        @Req() req: Request
+    ): Promise<LoginInfoDto | ApiResponse> {
+        const user = await this.userService.getByEmail(data.email);
+
+        if (!user) {
+            return new Promise(resolve => resolve(new ApiResponse('error', -3001)));
+        }
+
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        if (user.passwordHash !== passwordHashString) {
+            return new Promise(resolve => resolve(new ApiResponse('error', -3002)));
+        }
+
+        const jwtData = new JwtDataDto();
+        jwtData.role = "user";
+        jwtData.id = user.userId;
+        jwtData.identity = user.email;
+
+        let sada = new Date();
+        sada.setDate(sada.getDate() + 14);
+        const istekTimestamp = sada.getTime() / 1000;
+        jwtData.exp = istekTimestamp;
+
+        jwtData.ip = req.ip.toString();
+        jwtData.ua = req.headers["user-agent"];
+
+        let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret);
+
+        const responseObject = new LoginInfoDto(
+            user.userId,
+            user.email,
+            token
+        );
+
+        return new Promise(resolve => resolve(responseObject));
+    }
+
 }
